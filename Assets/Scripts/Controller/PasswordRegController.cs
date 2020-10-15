3cc82df9e7c217378e108b7036167d746e3f24b5
Hayden <55818671+GravitysAndroid@users.noreply.github.com>
2020-10-15 15:47:28 +1300

    Fixed the errors

--- Assets/Scripts/Controller/PasswordRegController.cs
+++ Assets/Scripts/Controller/PasswordRegController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class PasswordRegController : MonoBehaviour
+public class PasswordRegisterController : MonoBehaviour
 {
     public GameObject RegPanel;
     public GameObject PasspromptPanel;
